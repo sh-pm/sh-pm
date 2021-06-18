133b83e7acb9906dd912d9c2c49126a01b9e29f7
sh-pm <shpmmail@gmail.com>
2021-06-17 23:03:38 -0300

    v4.0.0
    - Update template pom

--- src/main/resources/template_pom.sh
+++ src/main/resources/template_pom.sh
@@ -3,7 +3,7 @@ ARTIFACT_ID=
 VERSION=v0.1.0
 
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v3.3.0@github.com/sh-pm
+    [sh-pm]=v4.0.0@github.com/sh-pm
     [sh-logger]=v1.4.0@github.com/sh-logger \
     [sh-unit]=v1.5.4@@github.com/sh-unit \
     [sh-commons]=v1.7.1@github.com/sh-commons \    
