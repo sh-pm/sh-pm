48e665998eb249fdc0a2cc500a8f7f57792a780b
sh-pm <shpmmail@gmail.com>
2021-06-17 22:58:03 -0300

    v4.0.0
    - ShellCheck revision in all code;
    - sh-pm pass use shellcheck if it exists when run tests;
    - Update pass to use only git repositories;
    - Remove REPOSITORY from pom.sh;
    - Add release 4.0.0 in releases folder;
    - Update changelog

--- pom.sh
+++ pom.sh
@@ -1,10 +1,3 @@
 GROUP_ID=bash
 ARTIFACT_ID=sh-pm
-VERSION=v3.3.0
-
-declare -A REPOSITORY=( \
-	[host]="shpmcenter.com" \
-	[port]=443 \
-);
-
-SSO_API_AUTHENTICATION_URL=https://shpmcenter.com/sso/rest/api/sso/authentication
+VERSION=v4.0.0
--- src/main/resources/template_pom.sh
+++ src/main/resources/template_pom.sh
@@ -2,14 +2,9 @@ GROUP_ID=bash
 ARTIFACT_ID=
 VERSION=v0.1.0
 
-declare -A REPOSITORY=( \
-	[host]="shpmcenter.com" \
-	[port]=443 \
-);
-
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v3.3.0
-    [sh-logger]=v1.4.0 \
-    [sh-unit]=v1.5.4 \
-    [sh-commons]=v1.7.1 \    
+    [sh-pm]=v3.3.0@github.com/sh-pm
+    [sh-logger]=v1.4.0@github.com/sh-logger \
+    [sh-unit]=v1.5.4@@github.com/sh-unit \
+    [sh-commons]=v1.7.1@github.com/sh-commons \    
 );
