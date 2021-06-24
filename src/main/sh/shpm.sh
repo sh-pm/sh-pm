7a2e6884dd907c91592c7ba4fd6a00b8b4ac8641
sh-pm <shpmmail@gmail.com>
2021-06-24 13:30:23 -0300

    v4.2.0
    - Enhancements in log of coverage analysis

--- src/main/sh/shpm.sh
+++ src/main/sh/shpm.sh
@@ -976,80 +976,157 @@ run_coverage_analysis() {
 	else
 	    shpm_log "Test Coverage OK: $COVERAGE_STR_LOG" "green"
 	fi
+	
+	shpm_log ""
 }
 
 do_coverage_analysis() {
 	VERBOSE="$1"
 
-	FILE_COUNT=0
-	FUNCTIONS_COUNT=0
-	FUNCTIONS_WITH_TEST_COUNT=0
+	local TOTAL_FILES_ANALYSED_COUNT	
+	local TOTAL_FUNCTIONS_FOUNDED_COUNT
+	local TOTAL_FUNCTIONS_WITH_TEST_COUNT
+	local TOTAL_COVERAGE
+	local FILE_FUNCTIONS_COUNT
+	local FILE_FUNCTIONS_WITH_TEST_COUNT
+	local FILES_ANALYSIS_LOG_SEPARATOR
+
+	TOTAL_FILES_ANALYSED_COUNT=0
+	TOTAL_FUNCTIONS_FOUNDED_COUNT=0
+	TOTAL_FUNCTIONS_WITH_TEST_COUNT=0
+	TOTAL_COVERAGE=0
+	FILE_FUNCTIONS_COUNT=0
+	FILE_FUNCTIONS_WITH_TEST_COUNT=0
+	
+	FILES_ANALYSIS_LOG_SEPARATOR="----------------------------------------------------------------"
 	
 	if [[ "$VERBOSE" != "-v"  ]]; then
 		SHPM_LOG_DISABLED="$TRUE"
 	fi
 	
 	shpm_log ""
-	shpm_log "Run test coverage analysis in $SRC_DIR_PATH:"
+	shpm_log "Find src file/functions in SRC_DIR_PATH and respective tests file/functions in TEST_DIR_PATH:"
+	shpm_log "  * SRC_DIR_PATH: $SRC_DIR_PATH"
+	shpm_log "  * TEST_DIR_PATH: $TEST_DIR_PATH"
+	shpm_log ""
+	shpm_log "Start test coverage analysis ..."
 	shpm_log ""
 	
-	SH_FILES_FOUNDED=$( find "$SRC_DIR_PATH" -name "*.sh" )
 	
-	for filepath in "${SH_FILES_FOUNDED[@]}"; do 
-		FILE_COUNT=$((FILE_COUNT+1))
+	while IFS=  read -r -d $'\0'; do
+    	SH_FILES_FOUNDED+=("$REPLY")
+	done < <(find "$SRC_DIR_PATH" -name "*.sh" -print0)
+	
+	TOTAL_FILES_ANALYSED_COUNT="${#SH_FILES_FOUNDED[@]}"
+	
+	shpm_log "$FILES_ANALYSIS_LOG_SEPARATOR"
+	
+	for i in "${!SH_FILES_FOUNDED[@]}"; do 
+	
+	    filepath="${SH_FILES_FOUNDED[$i]}"
+	     
+		FILE_FUNCTIONS_COUNT=0
+		FILE_FUNCTIONS_WITH_TEST_COUNT=0
 		 
-		shpm_log ""
-		shpm_log "-- $filepath ---------------------- "
+		increase_g_indent 
+		filename="$( basename "$filepath" )"
 		
 		FUNCTIONS_TO_TEST=( $(grep -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)' "$filepath" | tr \(\)\}\{ ' ' | sed 's/^[ \t]*//;s/[ \t]*$//') );
-		  
-		filename="$( basename "$filepath" )"
+		FILE_FUNCTIONS_COUNT="${#FUNCTIONS_TO_TEST[@]}"
+		TOTAL_FUNCTIONS_FOUNDED_COUNT=$(( TOTAL_FUNCTIONS_FOUNDED_COUNT + FILE_FUNCTIONS_COUNT )) 
+		
 		test_filename="${filename//.sh/}_test.sh"
 		test_filepath="$TEST_DIR_PATH/$test_filename"
 		 
+		shpm_log "FILE: $filename - Analysis Start"
+		
+		shpm_log " - Location: $filepath"
 		if [[ -f "$test_filepath" ]]; then
+	
+			shpm_log " - TestedBy: $test_filepath" 
 			EXISTING_TEST_FUNCTIONS=( $(grep -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)' "$test_filepath" | tr \(\)\}\{ ' ' | sed 's/^[ \t]*//;s/[ \t]*$//') );
+			
+			shpm_log " - Function's coverage analysis:"
+			
+			increase_g_indent 
+			increase_g_indent
+			increase_g_indent
+			
+			FILE_FUNCTIONS_WITH_TEST_COUNT=0
+			
+			for function_name in "${FUNCTIONS_TO_TEST[@]}"; do
+				
+				foundtest=$FALSE
+				for test_function in "${EXISTING_TEST_FUNCTIONS[@]}"; do
+					if [[ "$test_function" == "test_""$function_name" ]]; then
+						foundtest=$TRUE
+						break;
+					fi
+				done;
+				
+				if [[ "$foundtest" == "$FALSE" ]]; then
+				   shpm_log "$function_name ... NO TEST FOUND!" "red"
+				else
+					FILE_FUNCTIONS_WITH_TEST_COUNT=$((FILE_FUNCTIONS_WITH_TEST_COUNT + 1))
+				   shpm_log "$function_name ... OK, test found." "green"
+				fi
+				
+			done
+			
+			TOTAL_FUNCTIONS_WITH_TEST_COUNT=$(( TOTAL_FUNCTIONS_WITH_TEST_COUNT + FILE_FUNCTIONS_WITH_TEST_COUNT ))
+			
+			decrease_g_indent 
+			decrease_g_indent
+			decrease_g_indent
+			
+		else		
+			shpm_log " - TestedBy: NO FILE TEST FOUND!" "red"
+			
+			increase_g_indent 
+			increase_g_indent
+			increase_g_indent
+			
+			for function_name in "${FUNCTIONS_TO_TEST[@]}"; do
+				shpm_log "$function_name ... NO TEST FOUND!" "red"
+			done
+			
+			decrease_g_indent 
+			decrease_g_indent
+			decrease_g_indent
 		fi
 		
-		FILE_FUNCTIONS_COUNT=0
-		FILE_FUNCTIONS_WITH_TEST_COUNT=0
-		for function_name in "${FUNCTIONS_TO_TEST[@]}"; do
-			FILE_FUNCTIONS_COUNT=$((FILE_FUNCTIONS_COUNT+1))
-			FUNCTIONS_COUNT=$((FUNCTIONS_COUNT+1))
-			
-			foundtest=$FALSE
-			for test_function in "${EXISTING_TEST_FUNCTIONS[@]}"; do
-				if [[ "$test_function" == "test_""$function_name" ]]; then
-					foundtest=$TRUE
-					FILE_FUNCTIONS_WITH_TEST_COUNT=$((FILE_FUNCTIONS_WITH_TEST_COUNT+1))
-					FUNCTIONS_WITH_TEST_COUNT=$((FUNCTIONS_WITH_TEST_COUNT+1))
-					break;
-				fi
-			done;
-			
-			if [[ "$foundtest" == "$FALSE" ]]; then
-			   shpm_log "     $function_name ... No tests found!" "red"
-			else
-			   shpm_log "     $function_name ... OK" "green"
-			fi
-		done
-		
-		PERCENT_COVERAGE=$(bc <<< "scale=2; $FILE_FUNCTIONS_WITH_TEST_COUNT / $FILE_FUNCTIONS_COUNT * 100")
+		if [ $TOTAL_FUNCTIONS_WITH_TEST_COUNT -gt 0 ]; then 
+			PERCENT_COVERAGE=$(bc <<< "scale=2; $TOTAL_FUNCTIONS_WITH_TEST_COUNT / $TOTAL_FUNCTIONS_FOUNDED_COUNT * 100")
+		else
+			PERCENT_COVERAGE=0
+		fi
 		
 		shpm_log ""
-		shpm_log "   Found $FILE_FUNCTIONS_COUNT functions in $filepath."
+		shpm_log "Found $FILE_FUNCTIONS_COUNT function(s) in $filename. $FILE_FUNCTIONS_WITH_TEST_COUNT function(s) have tests."
+		shpm_log ""
+		shpm_log "FILE: $filename - Analysis End"
 	
+		decrease_g_indent
+		
+		shpm_log "$FILES_ANALYSIS_LOG_SEPARATOR"
 	done
 	
-	shpm_log "--------------------------"
+	if [ $TOTAL_FUNCTIONS_WITH_TEST_COUNT -gt 0 ]; then 
+		TOTAL_COVERAGE=$(bc <<< "scale=2; $TOTAL_FUNCTIONS_WITH_TEST_COUNT / $TOTAL_FUNCTIONS_FOUNDED_COUNT * 100")
+	else
+		TOTAL_COVERAGE=0
+	fi
+	
 	shpm_log ""
-	shpm_log "Found $FUNCTIONS_COUNT functions in $FILE_COUNT file(s) analysed."
+	shpm_log "Finish test coverage analysis in $SRC_DIR_PATH:"
+	shpm_log ""
+	shpm_log "Found $TOTAL_FUNCTIONS_FOUNDED_COUNT function(s) in $TOTAL_FILES_ANALYSED_COUNT file(s) analysed. $TOTAL_FUNCTIONS_WITH_TEST_COUNT function(s) have tests."
 	shpm_log ""
 	
 	shpm_log "Coverage in %:"
 	SHPM_LOG_DISABLED="$FALSE"
 	
-	echo "$PERCENT_COVERAGE" # this is a "return" value for this function	
+	echo "$TOTAL_COVERAGE" # this is a "return" value for this function	
 }
 
 compile_sh_project() {
