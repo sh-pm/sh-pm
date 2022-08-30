43733772e7608be3ecce370d21c491c68a24e525
Arildo <arildo.gueno@gmail.com>
2022-08-30 10:59:57 -0300

    v5.1.0
    - Remove HANDLED_FILE_ENTRYPOINT_PATH;
    - Rename template dependencies file;
    - Update sh-unit dep in deps.sh.

--- deps.sh
+++ deps.sh
@@ -5,5 +5,5 @@ VERSION=v5.1.0
 
 declare -A DEPENDENCIES=( \
     [sh-logger]=v1.4.0@github.com/sh-pm \
-    [sh-unit]=v1.5.9@github.com/sh-pm \
+    [sh-unit]=v1.6.0@github.com/sh-pm \
 );
--- src/main/resources/template_deps.sh
+++ src/main/resources/template_deps.sh
@@ -4,8 +4,8 @@ ARTIFACT_ID=
 VERSION=v0.1.0
 
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v4.1.0@github.com/sh-pm \
+    [sh-pm]=v5.1.0@github.com/sh-pm \
     [sh-logger]=v1.4.0@github.com/sh-pm \
-    [sh-unit]=v1.5.5@github.com/sh-pm \
+    [sh-unit]=v1.6.0@github.com/sh-pm \
     [sh-commons]=v2.2.3@github.com/sh-pm \
 );
